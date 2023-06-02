72e2d1a3ef1e379a4dc8bd5ffcc99de44443fd2c
Pedro Gabriel <pedrpg5@gmail.com>
2023-06-02 09:43:38 -0300

    Começando projeto

---
+++ server/database/migrations/2023_06_02_122340_create_expense_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->string('merchandise_purchased')->nullable(false);
+            $table->string('establishment')->nullable(false);
+            $table->integer('installments')->nullable(false);
+            $table->double('value_installment')->nullable(false);
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
--- server/database/migrations/2023_06_02_123746_create_income_table.php
+++ server/database/migrations/2023_06_02_123746_create_income_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('incomes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
+            $table->string('establishment')->nullable(false);
+            $table->integer('installments')->nullable(false);
+            $table->double('value_installment')->nullable(false);
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('incomes');
     }
 };
