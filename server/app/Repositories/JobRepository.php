db137834e539ecab4b601eb581133b9cf32ce4ac
Pedro Gabriel <pedrpg5@gmail.com>
2023-06-05 13:08:00 -0300

    Criando Trabalho

--- server/app/Repositories/JobRepository.php
+++ server/app/Repositories/JobRepository.php
@@ -2,9 +2,21 @@
 
 namespace App\Repositories;
 
+use App\Models\Job;
+use Illuminate\Support\Facades\DB;
+
 class JobRepository 
 {
     public static function create(array $args){
-        
+        return DB::transaction(function () use($args){
+            $newJob = Job::create([
+                'description' => $args['job']['description']? $args['job']['description'] : '',
+                'wage' => $args['job']['wage'],
+                'establishment' => $args['job']['establishment'],
+                'user_id' => $args['job']['user_id']
+            ]);
+
+            return $newJob;
+        });
     }
 }
--- server/app/Services/JobService.php
+++ server/app/Services/JobService.php
@@ -2,9 +2,33 @@
 
 namespace App\Services;
 
+use App\Repositories\JobRepository;
+
 class JobService
 {
     public static function createJob(array $args){
-        dd($args);
+        try {
+            $newJob = JobRepository::create($args);
+            
+            if(!$newJob){
+                return [
+                   'code' => 500,
+                   'message' => "Falha ao criar o trablaho!" 
+                ];
+            }
+
+            return [
+                'code' => 200,
+                'message' => "Trabalho criado com sucesso!",
+                'job' => $newJob 
+             ];
+
+        } catch (\Throwable $th) {
+            return [
+                'code' => 500,
+                'message' => "Falha ao criar o trablaho!" 
+             ];
+        }
+        
     }
 }
