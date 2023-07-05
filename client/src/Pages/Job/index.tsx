e04df27d4e410f4b2ad5eca8e89037b1404c88de
Pedro Gabriel <pedrpg5@gmail.com>
2023-07-05 15:53:38 -0300

    Começando página de Trabalhos

---
+++ client/src/Pages/Job/index.tsx
@@ -0,0 +1,11 @@
+import { JobStyle } from "./style";
+
+const Job = () => {
+    return (
+        <JobStyle>
+            <h1>JOBS</h1>
+        </JobStyle>
+    );
+}
+
+export default Job;
--- client/src/Routes/index.tsx
+++ client/src/Routes/index.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../Pages/Auth/Login";
 import Singin from "../Pages/Auth/Singin";
 import Home from "../Pages/Home";
+import Job from "../Pages/Job";
 
 const Rotas = () => {
     return(
         <Routes>
             <Route path="/login" element={<Login />}/>
             <Route path="/singin" element={<Singin />}/>
+            <Route path="/jobs" element={<Job />}/>
             <Route path="/" element={<Home />}/>
         </Routes>
     );
